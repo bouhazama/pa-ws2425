6ef739ee126f862262af46228186e90dc83ca566
Youssef <youssef.bouhazama@stud.tu-darmstadt.de>
2025-02-13 14:13:46 +0100

    declare file_path, brewing, tank_id, and measured_quantites variables

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import numpy as np
 
 import project.functions as fn
 
+file_path = "./project/data/data_GdD_Datensatz_WS2425.h5"
+brewing = "brewing_0002" 
+tank_id = "B001"
 
 def main():
     pass
