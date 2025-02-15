3e97760343c9ae51ee36f487da151211434775bb
Youssef <youssef.bouhazama@stud.tu-darmstadt.de>
2025-02-15 11:40:26 +0100

    read brewing group related metadata

--- main.py
+++ main.py
@@ -11,8 +11,13 @@ tank_id = "B001"
 measured_quantities = ("level", "temperature", "timestamp")
 
 def main():
-    unit_metadata = fn.read_metadata(file_path, "brewing_0002/B001/level", "unit")
+    brewing_T_env = fn.read_metadata(file_path, brewing, "T_env")
+    brewing_specific_heat_capacity_beer = fn.read_metadata(file_path, brewing, "specific_heat_capacity_beer")
+    brewing_density_beer = fn.read_metadata(file_path, brewing, "density_beer")
 
+    print(brewing_T_env)
+    print(brewing_specific_heat_capacity_beer)
+    print(brewing_density_beer)
 
 if __name__ == "__main__":
     main()
